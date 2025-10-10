f4cd6d3b2aca429c987fe15cdabf96279f59badd
PheanoukHun <pheanouk.h@gmail.com>
2025-10-10 10:03:53 -0500

    Started Project 6

---
+++ UniUnison/project06/src/HomeField.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+ * CS312 Assignment 6 - HomeFieldAdvantage
+ * 
+ * On my honor, <NAME>, this programming assignment is my own work, 
+ * I have not shared it with others and I will not share it in the future.
+ *
+ * A program to play determine the extend of home field advantage in sports.
+ *
+ *  Name:
+ *  UTEID:
+ *  
+ */
+
+ 
+ //  Data from 14 input files and analysis of Home Field Advantate go here. 
+ 
+ 
+public class HomeField {
+
+    // Ask the user for the name of a data file and process
+    // it until they want to quit.
+    public static void main(String[] args) throws IOException {
+        System.out.println("A program to analyze home field advantage in sports.");
+        System.out.println();
+        // CS312 students. Do not create any other Scanners connected to System.in.
+        // Pass keyboard as a parameter to all the methods that need it. 
+        Scanner keyboard = new Scanner(System.in);
+
+        // CS312 students - Add your code here
+        
+        keyboard.close();
+    }
+
+    // CS312 Students - Add your methods here.
+}
