4e8b1beabc41cb3946d693e7a0a898bd085f54f4
PheanoukHun <pheanouk.h@gmail.com>
2025-11-02 11:09:03 -0600

    Correct Output, just need formatting

--- UniUnison/project08/src/Decrypt.java
+++ UniUnison/project08/src/Decrypt.java
@@ -1,24 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * CS312 Assignment 8 - Decrypt
  * 
- * On my honor, <NAME>, this programming assignment is my own work, 
+ * On my honor, Pheanouk Hun, this programming assignment is my own work,
  * I have not shared it with others and I will not share it in the future.
  *
- * Program to decrypt a message that has been encrypted with a substitution cipher.
- * We assume only characters with ASCII codes from 32 to 126 inclusive have been encrypted.
+ * Program to decrypt a message that has been encrypted with a substitution
+ * cipher.
+ * We assume only characters with ASCII codes from 32 to 126 inclusive have been
+ * encrypted.
  * 
- * Name:
- * UTEID:
+ * Name: Pheanouk Hun
+ * UTEID: ph23434
  * 
  */
 
 public class Decrypt {
 
-    // CS312 students, add your constants here: (delete this comment in final version of program)
-
-
+    // Initializing the Constants Values
+    public static final int ASCII_LEN = 128;
+    public static final int SPACE = 32;
+    public static final int TILDE = 126;
 
     public static void main(String[] arg) {
         // CS312 Students, do not create any other Scanners connected to System.in
@@ -26,26 +30,150 @@ public class Decrypt {
         String fileName = getFileName(keyboard);
         String encryptedText = DecryptUtilities.convertFileToString(fileName);
 
-        // The other method from DecryptUtilities you will have to use is
-        // DecryptUtilities.getDecryptionKey(int[]), but first you need to
-        // create an array with the frequency of all the ASCII characters in the
-        // encrypted text. Count ALL characters from ASCII code 0 to ASCII code 127, and
-        // pass it as a parameter to getDecryptionKey.
-        // (delete this comment in final version of program)
+        boolean wantToSwap = true;
 
-        // CS312 students, add you code here. (delete this comment in final version of program)
+        System.out.println("The encrypted text is:\n" + encryptedText);
 
+        int[] charFrequencies = new int[ASCII_LEN];
+        getCharFrequencies(charFrequencies, encryptedText);
 
+        char[] keySet = DecryptUtilities.getDecryptionKey(charFrequencies);
+
+        printFrequency(charFrequencies);
+        printEncryptionKeySet(keySet);
+
+        while (wantToSwap) {
+
+            wantToSwap = getWantToSwap(keyboard);
+
+            if (wantToSwap) {
+                switchKeys(keyboard, keySet);
+            }
+        }
+
+        printEncryptionKeySet(keySet);
         keyboard.close();
     }
 
-    // CS312 students, add your methods here: (delete this comment in final version of program)
+    /**
+     * 
+     * @param keyboard
+     * @param prompt
+     * @return
+     */
+    public static char getCharForSwitching(Scanner keyboard, String prompt) {
+        System.out.print(prompt);
+        String userChoice = keyboard.nextLine();
+        return (userChoice.charAt(0));
+    }
 
+    /**
+     * 
+     * @param keyboard
+     * @param keySet
+     */
+    public static void switchKeys(Scanner keyboard, char[] keySet) {
 
+        String promptOne = "Enter the decrypt character you want to change: ";
+        char oldChar = getCharForSwitching(keyboard, promptOne);
 
+        String promptTwo = "Enter what the character " + oldChar + " should decrypt to instead: ";
+        char newChar = getCharForSwitching(keyboard, promptTwo);
 
+        System.out.println(oldChar + "'s will now decrypt to " + newChar + "'s and vice versa.\n");
 
-    // Get the name of file to use. 
+        for (int i = 0; i < keySet.length; i++) {
+            char currChar = keySet[i];
+            if (currChar == oldChar) {
+                keySet[i] = newChar;
+            } else if (currChar == newChar) {
+                keySet[i] = oldChar;
+            }
+        }
+    }
+
+    /**
+     * 
+     * @param charFrequencies
+     */
+    public static void printFrequency(int[] charFrequencies) {
+
+        System.out.println("Frequencies of characters.\n" + "Character - Frequency");
+
+        for (int i = SPACE; i <= TILDE; i++) {
+            char currChar = (char) i;
+            int freq = charFrequencies[i];
+            System.out.println(currChar + " - " + freq);
+        }
+
+        System.out.println();
+    }
+
+    /**
+     * 
+     * @param keySet
+     */
+    public static void printEncryptionKeySet(char[] keySet) {
+
+        System.out.println("The current version of the key for ASCII characters 32 to 126 is: ");
+
+        for (int i = SPACE; i <= TILDE; i++) {
+
+            char encryptedChar = (char) i;
+            char decryptedChar = keySet[i];
+
+            System.out.print("Encrypt character: " + encryptedChar);
+            System.out.println(", decrypt character: " + decryptedChar);
+        }
+
+        System.out.println();
+    }
+
+    /**
+     * 
+     * @param encryptedText
+     * @param keySet
+     */
+    public static void printDecryptedText(String encryptedText, char[] keySet) {
+
+        System.out.println("The current version of the decrypted text is:\n");
+
+        for (int i = 0; i < encryptedText.length(); i++) {
+            int swappedIndex = (int) encryptedText.charAt(i);
+            char newChar = keySet[swappedIndex];
+            System.out.print(newChar);
+        }
+
+        System.out.println();
+    }
+
+    /**
+     * 
+     * @param keyboard
+     * @return
+     */
+    public static boolean getWantToSwap(Scanner keyboard) {
+        System.out.println("Do you want to make a change to the key?");
+        System.out.print("Enter 'Y' or 'y' to make change: ");
+
+        String userChoice = keyboard.nextLine();
+        char cleanedChoice = userChoice.toLowerCase().charAt(0);
+        return (cleanedChoice == 'y');
+    }
+
+    /**
+     * 
+     * @param charFrequencies
+     * @param encryptedText
+     */
+    public static void getCharFrequencies(int[] charFrequencies, String encryptedText) {
+        for (int i = 0; i < encryptedText.length(); i++) {
+            int currCharIndex = (int) encryptedText.charAt(i);
+            charFrequencies[currCharIndex]++;
+        }
+    }
+
+    // Get the name of file to use.
     public static String getFileName(Scanner keyboard) {
         System.out.print("Enter the name of the encrypted file: ");
         String fileName = keyboard.nextLine().trim();
