9962b862b9a7c92e0c786dbb8ee4c0496455ab83
Pheanouk <pheanouk.h@gmail.com>
2026-01-27 22:30:02 -0600

    Backup

--- UniUnison/CS314/assignment2/testings/multiplyMatrix.py
+++ UniUnison/CS314/assignment2/testings/multiplyMatrix.py
@@ -1,13 +1,19 @@
-arr1 = [[2, 4], [5, 2], [1, 5], [3, 6]]
-arr2 = [[3, 4, 1], [1, 8, 4]]
+import random
+import time
 
-arr3 = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+arr1 = [[random.randint(1,10) for i in range(500)] for i in range(1000)]
+arr2 = [[random.randint(1,10) for i in range(1000)] for i in range(500)]
 
+arr3 = [[0 for i in range (750)] for i in range(1000)]
+
+start = time.time()
 for i in range(len(arr1)):
     for j in range(len(arr2[0])):
         sum = 0
         for k in range(len(arr1[0])):
             sum += arr1[i][k] * arr2[k][j]
         arr3[i][j] = sum
+    print(i, end=", ")
+end = time.time()
 
-print(*arr3, sep="\n")
+print(f"\nRuntime: {end - start} seconds")
