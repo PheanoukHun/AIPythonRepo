04c417ff7e66467364cd44d0f748fa4d498792cf
Pheanouk <pheanouk.h@gmail.com>
2026-01-31 11:30:02 -0600

    Backup

---
+++ UniUnison/CS314/assignment3/src/NameSurfer.java
@@ -0,0 +1,287 @@
+/*
+ * NameSurfer.java - CS 314 Assignment 3
+ *
+ * By signing my name below, I affirm that this assignment is my own work. I
+ * have neither given nor received unauthorized assistance on this assignment.
+ *
+ * Name:
+ * Email address:
+ * UTEID:
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class NameSurfer {
+
+    // TODO: explain your novel menu option here
+
+    // TODO: explain your interesting search / trend here
+
+    // TODO: add test code for NameRecord class here
+
+
+    // One of the basic data files given on the assignment.
+    // Alter this to try different data files.
+    private static final String NAME_FILE = "names.txt";
+
+    // A few simple tests for the Names and NameRecord class.
+    public static void simpleTest() {
+        // raw data for Jake. Alter as necessary for your NameRecord
+        String jakeRawData = "Jake 262 312 323 479 484 630 751 453 225 117 97";
+        int baseDecade = 1900;
+        NameRecord jakeRecord =  // complete with your constructor
+        String expected = "Jake\n1900: 262\n1910: 312\n1920: 323\n1930: 479\n1940: "
+                        + "484\n1950: 630\n1960: 751\n1970: 453\n1980: 225\n1990: "
+                        + "117\n2000: 97\n";
+        String actual = jakeRecord.toString();
+        System.out.println("expected string:\n" + expected);
+        System.out.println("actual string:\n" + actual);
+        if (expected.equals(actual)) {
+            System.out.println("passed Jake toString test.");
+        } else {
+            System.out.println("FAILED Jake toString test.");
+        }
+
+        // Some getName Tests
+        Names names = new Names(getFileScannerForNames(NAME_FILE));
+        String[] testNames = {"Isabelle", "isabelle", "sel",
+                "X1178", "ZZ", "via", "kelly"};
+        boolean[] expectNull = {false, false, true, true, true, true, false};
+        for (int i = 0; i < testNames.length; i++) {
+            performGetNameTest(names, testNames[i], expectNull[i]);
+        }
+    }
+
+    // Checks if given name is present in Names.
+    private static void performGetNameTest(Names names, String name,
+                                           boolean expectNull) {
+
+        System.out.println("Performing test for this name: " + name);
+        if (expectNull) {
+            System.out.println("Expected return value is null");
+        } else {
+            System.out.println("Expected return value is not null");
+        }
+        NameRecord result = names.getName(name);
+        if ((expectNull && result == null) || (!expectNull && result != null)) {
+            System.out.println("PASSED TEST.");
+        } else {
+            System.out.println("Failed test");
+        }
+    }
+
+    // main method. Driver for the whole program
+    public static void main(String[] args) {
+        // Delete the following line in the final version of your program.
+        simpleTest();
+
+        Scanner fileScanner = getFileScannerForNames(NAME_FILE);
+        Names namesDatabase = new Names(fileScanner);
+        fileScanner.close();
+        runOptions(namesDatabase);
+    }
+
+    /* pre: namesDatabase != null
+     * Ask user for options to perform on the given Names object.
+     * Creates a Scanner connected to System.in.
+     */
+    private static void runOptions(Names namesDatabase) {
+        Scanner keyboard = new Scanner(System.in);
+        MenuChoices[] menuChoices = MenuChoices.values();
+        MenuChoices menuChoice;
+        do {
+            showMenu();
+            int userChoice = getChoice(keyboard) - 1;
+            menuChoice = menuChoices[userChoice];
+            if(menuChoice == MenuChoices.SEARCH) {
+                search(namesDatabase, keyboard);
+            } else if (menuChoice == MenuChoices.ONE_NAME) {
+                oneName(namesDatabase, keyboard);
+            } else if (menuChoice == MenuChoices.APPEAR_ONCE) {
+                appearOnce(namesDatabase);
+            } else if (menuChoice == MenuChoices.APPEAR_ALWAYS) {
+                appearAlways(namesDatabase);
+            } else if (menuChoice == MenuChoices.ALWAYS_MORE) {
+                alwaysMore(namesDatabase);
+            } else if (menuChoice == MenuChoices.ALWAYS_LESS) {
+                alwaysLess(namesDatabase);
+            } else if (menuChoice == MenuChoices.STUDENT_SEARCH) {
+                // CS314 students, call your search method here
+            }
+        } while(menuChoice != MenuChoices.QUIT);
+        keyboard.close();
+    }
+
+    /* Create a Scanner and return connected to a File with the given name.
+     * pre: fileName != null
+     * post: Return a Scanner connected to the file or null
+     * if the File does not exist in the current directory.
+     */
+    private static Scanner getFileScannerForNames(String fileName) {
+        Scanner sc = null;
+        try {
+            sc = new Scanner(new File(fileName));
+        } catch (FileNotFoundException e) {
+            System.out.println("\n***** ERROR IN READING FILE ***** ");
+            System.out.println("Can't find this file "
+                    + fileName + " in the current directory.");
+            System.out.println("Error: " + e);
+            String currentDir = System.getProperty("user.dir");
+            System.out.println("Be sure " + fileName + " is in this directory: ");
+            System.out.println(currentDir);
+            System.out.println("\nReturning null from method.");
+            sc = null;
+        }
+        return sc;
+    }
+
+    /* Display the names that have appeared in every decade.
+     * pre: n != null
+     * post: print out names that have appeared in ever decade
+     */
+    private static void appearAlways(Names namesDatabase) {
+        if (namesDatabase == null) {
+            throw new IllegalArgumentException("The parameter namesDatabase cannot be null");
+        }
+
+    }
+
+    /* Display the names that have appeared in only one decade.
+     * pre: n != null
+     * post: print out names that have appeared in only one decade
+     */
+    private static void appearOnce(Names namesDatabase) {
+        if (namesDatabase == null) {
+            throw new IllegalArgumentException("The parameter"
+                    + " namesDatabase cannot be null");
+        }
+
+    }
+
+    /* Display the names that have gotten more popular
+     * in each successive decade.
+     * pre: n != null
+     * post: print out names that have gotten more popular in each decade
+     */
+    private static void alwaysMore(Names namesDatabase) {
+        if (namesDatabase == null) {
+            throw new IllegalArgumentException("The parameter"
+                    + " namesDatabase cannot be null");
+        }
+
+    }
+
+    /* Display the names that have gotten less popular
+     * in each successive decade.
+     * pre: n != null
+     * post: print out names that have gotten less popular in each decade
+     */
+    private static void alwaysLess(Names namesDatabase) {
+        if (namesDatabase == null) {
+            throw new IllegalArgumentException("The parameter"
+                    + " namesDatabase cannot be null");
+        }
+
+    }
+
+    /* Display the data for one name or state that name has never been ranked.
+     * pre: n != null, keyboard != null and is connected to System.in
+     * post: print out the data for n or a message that n has never been in the
+     * top 1000 for any decade
+     */
+    private static void oneName(Names namesDatabase, Scanner keyboard) {
+        // Note, no way to check if keyboard actually connected to System.in
+        // so we simply assume it is.
+        if (namesDatabase == null || keyboard == null) {
+            throw new IllegalArgumentException("The parameters cannot be null");
+        }
+
+    }
+
+    /* Display all names that contain a substring from the user
+     * and the decade they were most popular.
+     * pre: n != null, keyboard != null and is connected to System.in
+     * post: display the data for each name.
+     */
+    private static void search(Names namesDatabase, Scanner keyboard) {
+        // Note, no way to check if keyboard actually connected to System.in
+        // so we simply assume it is.
+        if (namesDatabase == null || keyboard == null) {
+            throw new IllegalArgumentException("The parameters cannot be null");
+        }
+
+    }
+
+    /* Get choice from the user keyboard != null and is connected to System.in
+     * return an int that is >= MenuChoices.SEARCH.ordinal()
+     *  and <= MenuChoices.QUIT.ordinal().
+     */
+    private static int getChoice(Scanner keyboard) {
+        // Note, no way to check if keyboard actually connected to System.in
+        // so we simply assume it is.
+        if (keyboard == null) {
+            throw new IllegalArgumentException("The parameter keyboard cannot be null");
+        }
+        int choice = getInt(keyboard, "Enter choice: ");
+        keyboard.nextLine();
+        // Add one due to zero based indexing of enums, but 1 based indexing of menu.
+        final int MAX_CHOICE = MenuChoices.QUIT.ordinal() + 1;
+        while (choice < 1  || choice > MAX_CHOICE) {
+            System.out.println();
+            System.out.println(choice + " is not a valid choice");
+            choice = getInt(keyboard, "Enter choice: ");
+            keyboard.nextLine();
+        }
+        return choice;
+    }
+
+    /* Ensure an int is entered from the keyboard.
+     * pre: s != null and is connected to System.in
+     * post: return the int typed in by the user.
+     */
+    private static int getInt(Scanner s, String prompt) {
+        // Note, no way to check if keyboard actually connected to System.in
+        // so we simply assume it is.
+        if (s == null) {
+            throw new IllegalArgumentException("The parameter s cannot be null");
+        }
+        System.out.print(prompt);
+        while (!s.hasNextInt()) {
+            s.next();
+            System.out.println("That was not an int.");
+            System.out.print(prompt);
+        }
+        return s.nextInt();
+    }
+
+    // Show the user the menu.
+    private static void showMenu() {
+        System.out.println();
+        System.out.println("Options:");
+        System.out.println("Enter 1 to search for names.");
+        System.out.println("Enter 2 to display data for one name.");
+        System.out.println("Enter 3 to display all names that appear in only "
+                + "one decade.");
+        System.out.println("Enter 4 to display all names that appear in all "
+                + "decades.");
+        System.out.println("Enter 5 to display all names that are more popular "
+                + "in every decade.");
+        System.out.println("Enter 6 to display all names that are less popular "
+                + "in every decade.");
+        System.out.println("Enter 7 to <replace with description of "
+                + "your method / search>.");
+        System.out.println("Enter 8 to quit.");
+        System.out.println();
+    }
+
+    /**
+     * An enumerated type to hold the menu choices
+     * for the NameSurfer program.
+     */
+    private static enum MenuChoices {
+        SEARCH, ONE_NAME, APPEAR_ONCE, APPEAR_ALWAYS, ALWAYS_MORE,
+        ALWAYS_LESS, STUDENT_SEARCH, QUIT;
+    }
+}
---
+++ UniUnison/CS314/assignment3/src/Names.java
@@ -0,0 +1,117 @@
+/*
+ * Names.java - CS 314 Assignment 3
+ *
+ * By signing my name below, I affirm that this assignment is my own work. I
+ * have neither given nor received unauthorized assistance on this assignment.
+ *
+ * Name:
+ * Email address:
+ * UTEID:
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * A collection of NameRecords.
+ * Stores NameRecord objects and provides methods to select NameRecords based
+ * on various criteria.
+ */
+public class Names {
+
+    /**
+     * Construct a new Names object based on the data source the Scanner sc is
+     * connected to. Assume the first two lines in the data source are the base
+     * year and number of decades to use. Any lines without the correct number
+     * of decades are discarded and are not part of the resulting Names object.
+     * Any names with ranks of all 0 are discarded and not part of the
+     * resulting Names object.
+     *
+     * @param sc Is connected to a data file with baby names
+     *           and positioned at the start of the data source.
+     */
+    public Names(Scanner sc) {
+
+    }
+
+    /**
+     * Returns an ArrayList of NameRecord objects that contain a given
+     * substring, ignoring case.  The names must be in sorted order based on
+     * the names of the NameRecords.
+     *
+     * @param partialName != null, partialName.length() > 0
+     * @return an ArrayList of NameRecords whose names contains partialName. If
+     * there are no NameRecords that meet this criteria, returns an empty list.
+     */
+    public ArrayList<NameRecord> getMatches(String partialName) {
+
+    }
+
+    /**
+     * Returns an ArrayList of Strings of names that have been ranked in the
+     * top 1000 or better for every decade. The Strings  must be in sorted
+     * order based on the name of the NameRecords.
+     *
+     * @return A list of the names that have been ranked in the top 1000 or
+     * better in every decade. The list is in sorted ascending order. If there
+     * are no NameRecords that meet this criteria, returns an empty list.
+     */
+    public ArrayList<String> rankedEveryDecade() {
+
+    }
+
+    /**
+     * Returns an ArrayList of Strings of names that have been ranked in the
+     * top 1000 or better in exactly one decade. The Strings must be in sorted
+     * order based on the name of the NameRecords.
+     *
+     * @return A list of the names that have been ranked in the top 1000 or
+     * better in exactly one decade. The list is in sorted ascending order. If
+     * there are no NameRecords that meet this criteria, returns an empty list.
+     */
+    public ArrayList<String> rankedOnlyOneDecade() {
+
+    }
+
+    /**
+     * Returns an ArrayList of Strings of names that have been getting more
+     * popular every decade. The Strings must be in sorted order based on the
+     * name of the NameRecords.
+     *
+     * @return A list of the names that have been getting more popular in every
+     * decade. The list is in sorted ascending order. If there are no
+     * NameRecords that meet this criteria, returns an empty list.
+     */
+    public ArrayList<String> alwaysMorePopular() {
+
+    }
+
+    /**
+     * Returns an ArrayList of Strings of names that have been getting less
+     * popular every decade. The Strings  must be in sorted order based on the
+     * name of the NameRecords.
+     *
+     * @return A list of the names that have been getting less popular in every
+     * decade. The list is in sorted ascending order. If there are no
+     * NameRecords that meet this criteria, returns an empty list.
+     */
+    public ArrayList<String> alwaysLessPopular() {
+
+    }
+
+    /**
+     * Return the NameRecord in this Names object that matches the given String
+     * ignoring case.
+     *
+     * pre: name != null
+     *
+     * @param name The name to search for.
+     * @return The name record with the given name or null if no NameRecord in
+     * this Names object contains the given name.
+     */
+    public NameRecord getName(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("The parameter name cannot be null");
+        }
+    }
+}
