c9c6b144baa9ddda0619b674b06d40b4933d3421
Pheanouk <pheanouk.h@gmail.com>
2026-02-02 15:45:04 -0600

    Device: Yoga; Backup: Mon Feb  2 03:45:01 PM CST 2026

--- UniUnison/CS314/assignment3/src/NameRecord.java
+++ UniUnison/CS314/assignment3/src/NameRecord.java
@@ -14,12 +14,21 @@ import java.util.Scanner;
 
 public class NameRecord implements Comparable<NameRecord> {
 
+    // Private Properties
     private String name;
     private int baseYear;
     private ArrayList<Integer> rankings;
     private int numRankedYears;
     private int numDecades;
 
+    // Constructor
+
+    /**
+     * 
+     * @param baseYear
+     * @param numRankedYears
+     * @param rawData
+     */
     public NameRecord(int baseYear, int numRankedYears, String rawData) {
 
         this.baseYear = baseYear;
@@ -38,19 +47,46 @@ public class NameRecord implements Comparable<NameRecord> {
         numDecades = rankings.size();
     }
 
+    // Accessor Methods
+
+    /**
+     * 
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * 
+     * @return
+     */
     public int getBaseYear() {
         return baseYear;
     }
 
+    /**
+     * 
+     * @return
+     */
     public int getNumDecades() {
         return numDecades;
     }
 
-    public int getSpecificDecadeRank(int decade) {
+    /**
+     * 
+     * @return
+     */
+    public int getNumRankedYears() {
+        return numRankedYears;
+    }
+
+    /**
+     * 
+     * @param decade - Int value; 0 <= decade < getNumDecades()
+     * @return
+     */
+    public int getDecadeNameRank(int decade) {
 
         // Checking Preconditions
         if (decade < 0 || decade >= getNumDecades()) {
@@ -61,19 +97,71 @@ public class NameRecord implements Comparable<NameRecord> {
         return rankings.get(decade);
     }
 
-    public int getNumRankedYears() {
-        return numRankedYears;
+    public int getMostPopularDecade() {
+        int mostPopularIndex = 0;
+
+        while (getDecadeNameRank(mostPopularIndex) == 0 && mostPopularIndex < getNumDecades()) {
+            mostPopularIndex++;
+        }
+
+        for (int i = mostPopularIndex; i < getNumDecades(); i++) {
+            if (getDecadeNameRank(i) <= mostPopularIndex) {
+                mostPopularIndex = i;
+            }
+        }
+
+        int mostRecentYear = getBaseYear() + (mostPopularIndex * 10);
+
+        return mostRecentYear;
+    }
+
+    public int getNumRankedTop1000() {
+        
+        int numTimes = 0;
+
+        for (int i = 0; i < getNumDecades(); i++) {
+            if (getDecadeNameRank(i) != 0) {
+                numTimes++;
+            }
+        }
+
+        return numTimes;
+    }
+
+    public boolean neverBelowTop1000() {
+
+        for (int i = 0; i < getNumDecades(); i++) {
+            if (getDecadeNameRank(i) == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean rankedInTop1000Once() {
+
+        int top1000NumTimes = 0;
+
+        for (int i = 0; i < getNumDecades(); i++) {
+            if (getDecadeNameRank(i) != 0) {
+                top1000NumTimes++;
+            }
+        }
+
+        return top1000NumTimes == 1;
     }
 
     public String toString() {
         StringBuilder results = new StringBuilder(name + "\n");
         for (int i = 0; i < getNumDecades(); i++) {
-            results.append((baseYear + (i * 10)) + ": " + getSpecificDecadeRank(i) + "\n");
+            results.append((baseYear + (i * 10)) + ": " + getDecadeNameRank(i) + "\n");
         }
         return results.toString();
     }
 
     public int compareTo(NameRecord other) {
-        return name.compareTo(other.getName());
+        int result = getName().compareTo(other.getName());
+        return result;
     }
 }
