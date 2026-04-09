62810ba3b751a9aca5e0c5e5b59e78d730bd04da
pheanoukhun <pheanouk.h@gmail.com>
2026-04-09 15:01:51 -0500

    Device: Yoga; Backup; Thu Apr  9 03:01:48 PM CDT 2026

--- UniUnison/CS314/assignment8/src/BSTTester.java
+++ UniUnison/CS314/assignment8/src/BSTTester.java
@@ -71,7 +71,6 @@ public class BSTTester {
     }
 
     private static void testSearchAndMinMax() {
-        
         // Setup
         BinarySearchTree<Integer> t = new BinarySearchTree<>();
         int[] vals = { 50, 10, 90, 5, 20 };
@@ -84,16 +83,16 @@ public class BSTTester {
 
         // Test 1: Element Not Present
         showTestResults(!t.isPresent(100), testNum++);
-        
+
         // Test 2: Element Present
         showTestResults(t.isPresent(20), testNum++);
 
         // min() Method
         System.out.println("\nmin() Method Testing: ");
-        
+
         // Test 1: min() from Original Values
         showTestResults(t.min() == 5, testNum++);
-        
+
         // Test 2: min() after adding a -5 Values
         t.add(-5);
         showTestResults(t.min() == -5, testNum++);
@@ -103,44 +102,42 @@ public class BSTTester {
 
         // Test 1: max() from Original Values
         showTestResults(t.max() == 90, testNum++);
-        
+
         // Test 2: max() after adding a 100 Values
         t.add(100);
         showTestResults(t.max() == 100, testNum++);
     }
 
     private static void testRemovalLogic() {
-
         // Setup
         BinarySearchTree<Integer> t = new BinarySearchTree<>();
-        int[] values = new int[]{50, 25, 75, 12, 30};
+        int[] values = new int[] { 50, 25, 75, 12, 30 };
         for (int value : values) {
             t.add(value);
         }
-        
+
         // remove() Method
         System.out.println("\nremove() Method Testing: ");
-        
+
         // Test 1: Remove Root Node
         showTestResults(t.remove(50) && t.size() == 4, testNum++);
-        
+
         // Test 2: Remove Leaf Node
         showTestResults(t.remove(12) && t.size() == 3, testNum++);
     }
 
     private static void testStructureMethods() {
-        
         // Setup
         BinarySearchTree<Integer> t = new BinarySearchTree<>();
 
         // Height Tests
         System.out.println("\nheight() Tests: ");
-        
+
         // Test 1: Height of empty tree
         showTestResults(t.height() == -1, testNum++);
 
         // Test 2: Height of non-trivial tree
-        int[] values = new int[]{50, 25, 75, 12, 30};
+        int[] values = new int[] { 50, 25, 75, 12, 30 };
         for (int value : values) {
             t.add(value);
         }
@@ -148,7 +145,7 @@ public class BSTTester {
 
         // numNodesAtDepth Tests
         System.out.println("\nnumNodesAtDepth() Tests: ");
-        
+
         // Test 1: Node at Largest Depth
         showTestResults(t.numNodesAtDepth(2) == 2, testNum++);
 
@@ -157,47 +154,65 @@ public class BSTTester {
 
         // get() Tests
         System.out.println("\nget() Tests: ");
-        
+
         // Test 1: get(kth) - Smallest
         showTestResults(t.get(0) == 12, testNum++);
 
         // Test 2: get(kth) - Middle
         showTestResults(t.get(3) == 50, testNum++);
+        
+        // size() Test
+        // Test 1: size() on empty tree
+        t = new BinarySearchTree<>();
+        showTestResults(t.size() == 0, testNum++);
+        
+        // Test 2: size() on non-empty tree
+        for (int i = 1; i <= 5; i++) {
+            t.add(i * 10);
+        }
+        showTestResults(t.size() == 5, testNum++);
     }
 
-    private static void getAllGreaterAndLessThan() {
-        
+    private static void testGetAllGreaterAndLessThan() {
         // Setup
         BinarySearchTree<Integer> t = new BinarySearchTree<>();
-        
-        
-        // Get all 
-        
-        
-        for (int i = 1; i <= 10; i++) t.add(i * 10); // 10, 20, 30... 100
 
-        // Test 17: getAll
+        // getAll() method Test
+        System.out.println("\ngetAll() Tests: ");
+
+        // Test 1: getAll with empty tree
+        List<Integer> allEmpty = t.getAll();
+        showTestResults(allEmpty.isEmpty(), testNum++);
+
+        // Test 2: getAll with non-empty tree
+        for (int i = 1; i <= 10; i++) {
+            t.add(i * 10);
+        }
+
         List<Integer> all = t.getAll();
-        showTestResults(all.size() == 10 && all.get(0) == 10, testNum++);
+        showTestResults(all.size() == 10, testNum++);
 
-        // Test 18: getAllLessThan
-        List<Integer> lessThan = t.getAllLessThan(35);
-        // Should be 10, 20, 30
-        showTestResults(
-            lessThan.size() == 3 && lessThan.get(2) == 30,
-            testNum++
-        );
+        // getAllGreaterAndLessThan Tests
+        System.out.println("\ngetAllGreaterThan() Tests: ");
+        
+        // Test 1: getAllGreaterThan with value greater than max value of tree.
+        List<Integer> greaterThanMax = t.getAllGreaterThan(110);
+        showTestResults(greaterThanMax.isEmpty(), testNum++);
+        
+        // Test 2: getAllGreaterThan with value less than min value of tree.
+        List<Integer> lessThanMin = t.getAllGreaterThan(5);
+        showTestResults(lessThanMin.isEmpty(), testNum++);
 
-        // Test 19: getAllGreaterThan
-        List<Integer> greaterThan = t.getAllGreaterThan(85);
-        // Should be 90, 100
-        showTestResults(
-            greaterThan.size() == 2 && greaterThan.get(0) == 90,
-            testNum++
-        );
-
-        // Test 20: getAllGreaterThan (None found)
-        showTestResults(t.getAllGreaterThan(200).isEmpty(), testNum++);
+        // getAllLessThan Tests
+        System.out.println("\ngetAllLessThan() Tests: ");
+        
+        // Test 1: getAllLessThan with value greater than max value of tree.
+        List<Integer> lessThanMax = t.getAllLessThan(110);
+        showTestResults(lessThanMax.isEmpty(), testNum++);
+        
+        // Test 2: getAllLessThan with value less than min value of tree.
+        List<Integer> greaterThanMin = t.getAllLessThan(5);
+        showTestResults(greaterThanMin.isEmpty(), testNum++);
     }
 
     private static void showTestResults(boolean passed, int testNum) {
