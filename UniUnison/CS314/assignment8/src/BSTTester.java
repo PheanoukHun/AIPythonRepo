5a9c2717cc41e1f48175202ce618b7cd9a53546e
pheanoukhun <pheanouk.h@gmail.com>
2026-04-08 22:45:02 -0500

    Device: Yoga; Backup; Wed Apr  8 10:45:00 PM CDT 2026

--- UniUnison/CS314/assignment8/src/BSTTester.java
+++ UniUnison/CS314/assignment8/src/BSTTester.java
@@ -40,13 +40,19 @@ public class BSTTester {
         
         System.out.println("\nAdd Method Testing: ");
         
-        ArrayList<String> 
+        ArrayList<String> expectedList = new ArrayList<>();
         BinarySearchTree<String> t = new BinarySearchTree<>();
 
         // Test 1: Recursive Add and Size
         
         Random r = new Random();
+        for (int i = 0; i < 99; i++) {
+            String val = r.nextInt(100) + "";
+            t.add(val);
+            expectedList.add(val);
+        }
         
+        expectedList.sort();
         
         showTestResults(t.size() == 3, testNum++);
 
