625e8bd8427a91ecf88f7c1c422d48d670cff657
PheanoukHun <pheanouk.h@gmail.com>
2026-07-02 18:45:02 -0500

    Backup: 2026-07-02 18:45

--- AINoteSumm/config.py
+++ AINoteSumm/config.py
@@ -1,9 +1,15 @@
 import json
+import os
 import pathlib
 
+# Load up Config
 try:
     with open(pathlib.Path("config/config.json"), "r") as file:
         data = json.load(file)
+
+    # Create Example Config along with
+    # Example Directory and Example System
+    # Prompt If config file is not found
 except FileNotFoundError:
     data = """
     {
@@ -24,9 +30,18 @@ except FileNotFoundError:
         "agent_mode": true
       }
     }
-
     """
+
+    # Create an Example System Prompt File
+    with open("system-prompt-file.md", "w") as file:
+        file.write("You are a Minimal Agent running Llama-Server Backup, Say Hello.")
+
+    try:
+        os.mkdir("model-dir")
+    except FileExistsError:
+        pass
+    
     with open(pathlib.Path("config/config.json"), "w") as file:
-        file.write(default_config)
+        file.write(data)
 except json.JSONDecodeError:
     print("Non-Valid Config File")
