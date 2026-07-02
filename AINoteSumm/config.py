52ef7b59d17e8770f2377a2de0c98b5d4e19bdbe
PheanoukHun <pheanouk.h@gmail.com>
2026-07-02 18:30:02 -0500

    Backup: 2026-07-02 18:30

--- AINoteSumm/config.py
+++ AINoteSumm/config.py
@@ -5,22 +5,26 @@ try:
     with open(pathlib.Path("config/config.json"), "r") as file:
         data = json.load(file)
 except FileNotFoundError:
-    default_config = """
+    data = """
     {
+      "encoding_format": "utf-8",
       "URL": {
         "baseURL": "http://127.0.0.1",
         "port": 8080,
-        "trailingURL": "/v1/chat/completions"
+        "trailingURL": "/v1/chat/completions",
+        "api_key": "NULL"
       },
-
-      "SYSTEM_PROMPT_FILE": "/home/procastoh/Git-Repos/launch-llama/optional/prompts/obisidian-summary.md",
-
-      "encoding": "utf-8",
-
-      "model" : {
-        "model_dir": "/home/procastoh/Git-Repos/launch-llama/optional/LLM-Models/LLM/"
+      "server_start_command": {
+        "SYSTEM_PROMPT_FILE": "system-prompt-file.md",
+        "model_dir": "model-dir",
+        "context_size": 100000,
+        "gpu_layers": 99,
+        "key_quant": "q4_0",
+        "value_quant": "q4_0",
+        "agent_mode": true
       }
     }
+
     """
     with open(pathlib.Path("config/config.json"), "w") as file:
         file.write(default_config)
