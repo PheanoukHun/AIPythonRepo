1b4c95ead7f256e3b332c8f29b47aaca5df6efbb
PheanoukHun <pheanouk.h@gmail.com>
2026-07-02 14:30:02 -0500

    Backup: 2026-07-02 14:30

--- AINoteSumm/config.py
+++ AINoteSumm/config.py
@@ -2,7 +2,27 @@ import json
 import pathlib
 
 try:
-    with open(pathlib.Path("config/config.json")) as file:
+    with open(pathlib.Path("config/config.json"), "r") as file:
         data = json.load(file)
-except:
-    
+except FileNotFoundError:
+    default_config = """
+    {
+      "URL": {
+        "baseURL": "http://127.0.0.1",
+        "port": 8080,
+        "trailingURL": "/v1/chat/completions"
+      },
+
+      "SYSTEM_PROMPT_FILE": "/home/procastoh/Git-Repos/launch-llama/optional/prompts/obisidian-summary.md",
+
+      "encoding": "utf-8",
+
+      "model" : {
+        "model_dir": "/home/procastoh/Git-Repos/launch-llama/optional/LLM-Models/LLM/"
+      }
+    }
+    """
+    with open(pathlib.Path("config/config.json"), "w") as file:
+        file.write(default_config)
+except json.JSONDecodeError:
+    print("Non-Valid Config File")
