3a9dcc1f6979213dba5d6b4627a0b6aed8d8e5b1
PheanoukHun <pheanouk.h@gmail.com>
2026-07-02 20:15:02 -0500

    Backup: 2026-07-02 20:15

--- AINoteSumm/config.py
+++ AINoteSumm/config.py
@@ -3,19 +3,27 @@ import json
 import os
 import pathlib
 
-# Load up Config
-try:
-    with open(pathlib.Path("config/config.json"), "r") as file:
-        data = json.load(file)
+class ConfigVal:
+    def __init__ (self):
+        self.get_config_settings()
 
-    # Create Example Config along with
-    # Example Directory and Example System
-    # Prompt If config file is not found
-except FileNotFoundError:
-    data = ""
-    
-    with open(pathlib.Path("config/config.json"), "w") as file:
-        file.write(data)
-        
-except json.JSONDecodeError:
-    
+    def get_config_settings(self):
+        # Load up Config
+        try:
+            with open(pathlib.Path("config/config.json"), "r") as file:
+                data = json.load(file)
+
+            # Create Example Config along with
+            # Example Directory and Example System
+            # Prompt If config file is not found
+        except FileNotFoundError:
+            data = ""
+
+            with open(pathlib.Path("config/config.json"), "w") as file:
+                file.write(data)
+
+        except json.JSONDecodeError:
+            print("Incorrect JSON Format")
+            exit(1)
+
+    def create_default_config_file()
