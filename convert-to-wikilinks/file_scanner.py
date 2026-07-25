b066d0578089fb4aac03aec9ce64c412a3ce3631
PheanoukHun <pheanouk.h@gmail.com>
2026-07-25 11:00:02 -0500

    Backup: 2026-07-25 11:00

--- convert-to-wikilinks/file_scanner.py
+++ convert-to-wikilinks/file_scanner.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from valid_path import PATH_VALIDITY, interpret_results, is_valid_path
 
 
-class FileScanner:
+class FindMarkdownFiles:
     def __init__(self, directory_path: str):
         self.directory_path: Path = Path(directory_path)
 
@@ -13,15 +13,19 @@ class FileScanner:
         interpret_results(is_valid_dir)
         if not self.directory_path.is_dir():
             raise NotADirectoryError(f"Not a directory: {self.directory_path}")
-        return [str(p) for p in self.directory_path.rglob("*") if p.is_file()]
+        return [str(p) for p in self.directory_path.rglob("*") if p.is_file() and p.suffix == ".md"]
 
 
 if __name__ == "__main__":
+    import signal
+    signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+    
     if len(sys.argv) != 2:
         print("\nUsage: python file_scanner.py <dir_path>\n")
         sys.exit(1)
 
     dir_path: str = sys.argv[1]
-
-    print("Files: ")
-    print(*FileScanner(dir_path).get_all_files(), sep="\n")
+    try:
+        files = FindMarkdownFiles(dir_path).get_all_files()
+        print("Files:", *files, sep="\n")
+    except Broke
