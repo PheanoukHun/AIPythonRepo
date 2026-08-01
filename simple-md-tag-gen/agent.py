6f2b0fc91b877c617166cf9e93b2d534c1a5470f
PheanoukHun <pheanouk.h@gmail.com>
2026-08-01 17:45:02 -0500

    Backup: 2026-08-01 17:45

--- simple-md-tag-gen/agent.py
+++ simple-md-tag-gen/agent.py
@@ -1,10 +1,13 @@
+from __future__ import annotations
+
 import json
 import os
 import sys
 from collections.abc import Callable
-from typing import Any, cast
+from typing import cast
 
 import openai
+from openai.resources.beta.responses.responses import OpenAI
 from openai.types.chat import (
     ChatCompletionFunctionToolParam,
     ChatCompletionMessageParam,
@@ -14,4 +17,16 @@ from openai.types.chat import (
     ChatCompletionToolParam,
 )
 from openai.types.shared_params import FunctionDefinition
+from typing_extensions import final
 
+
+class Agent:
+    def __init__(self, *, url: str, model: str, sys_prompt: str, api_key: str) -> None:
+        self.__client: OpenAI = openai.OpenAI(base_url=url, api_key=api_key)
+        self.__model: str = model
+        self.__messages = [
+            {
+                "role": "system",
+                "content": sys_prompt,
+            },
+        ]
